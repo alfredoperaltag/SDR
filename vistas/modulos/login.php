41719928ac1d139b3e4c969bf71ff03d58f936a3
YonathanR11 <yonathanr11.12@gmail.com>
2019-01-23 15:27:07 -0600

    fix login

--- vistas/modulos/login.php
+++ vistas/modulos/login.php
@@ -1,9 +1,12 @@
-
 <div class="login-area login-bg">
         <div class="container">
             <div class="login-box ptb--100">
                 <form method="post" novalidate="">
                     <div class="login-form-head">
+                    <div class="logo d-flex justify-content-around mb-3">
+                        <a href="inicio"><img class="d-inline-block" src="vistas/assets/images/icon/sep-logo.png" alt="logo"></a>
+                        <a href="inicio"><img class="d-inline-block" src="vistas/assets/images/icon/logo-tec.png" alt="logo"></a>
+                    </div>
                         <h4>Inicia Sesion</h4>
                         <p>Inicia sesion para acceder a las opciones del sistema.</p>
                     </div>
