073c1f9ec64bad05a5aecb4d40499e04f5a84590
YonathanR11 <yonathanr11.12@gmail.com>
2019-01-24 16:01:12 -0600

    add button info in table residentes

--- vistas/modulos/Residentes.php
+++ vistas/modulos/Residentes.php
@@ -30,6 +30,7 @@
                             <td>
                                 <div class="btn-group">
                                     <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-primary"><i class="fa fa-info"></i></button>
                                 </div>
                             </td>
                         </tr>
@@ -44,6 +45,7 @@
                             <td>
                                 <div class="btn-group">
                                     <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-primary"><i class="fa fa-info"></i></button>
                                 </div>
                             </td>
                         </tr>
