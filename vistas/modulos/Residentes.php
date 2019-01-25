c3baae6b61e03a21925f10c361ad94ff189e40f8
YonathanR11 <yonathanr11.12@gmail.com>
2019-01-25 16:03:03 -0600

    update residentes

--- vistas/modulos/Residentes.php
+++ vistas/modulos/Residentes.php
@@ -3,7 +3,7 @@
         <h2 class="ml-4">Residentes</h2>
         <div class="card-body">
             <!-- <h1 class="header-title">Usuarios</h1> -->
-            <button class="btn btn-primary mb-3" data-toggle="modal" data-target="#modalAgregarUsuario">Agregar Usuario</button>
+            <button class="btn btn-primary btn-xs mb-3" data-toggle="modal" data-target="#modalAgregarUsuario">Alta Residente <br> Informe Tecnico de<br> Residencias Profecionales</button>
             <div class="data-tables datatable-primary">
                 <table class="text-center tablaES">
                     <thead class="text-capitalize">
@@ -68,7 +68,7 @@ MODAL AGREGAR USUARIO
               CABEZA DEL MODAL
               ======================================-->
             <div class="modal-header">
-                <h5 class="modal-title">Agregar usuario</h5>
+                <h5 class="modal-title">Agregar Residente</h5>
                 <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
             </div>
             <form role="form" method="post" enctype="multipart/form-data">
@@ -81,13 +81,13 @@ MODAL AGREGAR USUARIO
 
                         <div class="form-row align-items-center">
                             <!-- ENTRADA PARA EL NUMERO DE CONTROL -->
-                            <div class="col-sm-6 my-1">
+                            <div class="col-sm-2 my-1">
                                 <label for="example-text-input" class="col-form-label">No. Control</label>
-                                <input class="form-control" type="text" name="nuevoNombre" placeholder="Numero de control"
-                                    required>
+                                <input class="form-control" type="number" name="nuevoNoControl" placeholder="No. Control"
+                                    required autocomplete="off">
                             </div>
                             <!-- ENTRADA PARA SELECCIONAR SU CARRERA -->
-                            <div class="col-sm-6 my-1">
+                            <div class="col-sm-5 my-1">
                                 <label class="col-form-label">Carrera</label>
                                 <select class="custom-select" required>
                                     <option value="">Selecionar carrera</option>
@@ -95,46 +95,69 @@ MODAL AGREGAR USUARIO
                                     <option value="II">Ingenieria Informatica</option>
                                 </select>
                             </div>
-                        </div>
+                            <!-- ENTRADA PARA SELECCIONAR PERIODO -->
+                            <div class="col-sm-3 my-1">
+                                <label class="col-form-label">Periodo</label>
+                                <select class="custom-select" required>
+                                    <option value="">Selecionar periodo</option>
+                                    <option value="EJ">Enero/Junio</option>
+                                    <option value="AD">Agosto/Diciembre</option>
+                                </select>
+                            </div>
 
+                            <div class="col-sm-2 my-1">
+                                    <label class="col-form-label">Periodo</label>
+                                    <?php
+                                    $cont = date('Y');
+                                    ?>
+                                    <select class="custom-select" id="sel1" required>
+                                    <?php while ($cont >= 2018) { ?>
+                                    <option name="anio" value="<?php echo($cont); ?>"><?php echo($cont); ?></option>
+                                    <?php $cont = ($cont-1); } ?>
+                                    </select>
+                                </div>
+                            </div>
+
+                        </div>
                         <div class="form-row align-items-center">
-                        <!-- ENTRADA PARA EL NOMBRE -->
-                        <div class="col-sm-4 my-1">
-                            <label for="example-text-input" class="col-form-label">Nombre</label>
-                            <input class="form-control" type="text" name="nuevoNombre" placeholder="Ingresar nombre completo"
-                                required>
+                            <!-- ENTRADA PARA EL NOMBRE -->
+                            <div class="col-sm-4 my-1">
+                                <label for="example-text-input" class="col-form-label">Nombre</label>
+                                <input class="form-control" type="text" name="nuevoNombre" placeholder="Nombre"
+                                    required autocomplete="off">
+                            </div>
+                            <!-- ENTRADA PARA EL APELLIDO PATERNO -->
+                            <div class="col-sm-4 my-1">
+                                <label for="example-text-input" class="col-form-label">Apellido Paterno</label>
+                                <input class="form-control" type="text" name="nuevoApellidoP" placeholder="Apellido Paterno"
+                                    required autocomplete="off">
+                            </div>
+                            <!-- ENTRADA PARA EL APELLIDO MATERNO -->
+                            <div class="col-sm-4 my-1">
+                                <label for="example-text-input" class="col-form-label">Apellido Materno</label>
+                                <input class="form-control" type="text" name="nuevoApellidoM" placeholder="Apellido Materno"
+                                    required autocomplete="off">
+                            </div>
                         </div>
-                        <!-- ENTRADA PARA EL APELLIDO PATERNO -->
-                        <div class="col-sm-4 my-1">
-                            <label for="example-text-input" class="col-form-label">Apellido Paterno</label>
-                            <input class="form-control" type="text" name="nuevoApellidoP" placeholder="Ingresar apellido paterno completo"
-                                required>
+                        <div class="form-row align-items-center">
+                            <!-- ENTRADA PARA SELECCIONAR SU SEXO -->
+                            <div class="col-sm-6 my-1">
+                                <label class="col-form-label">Sexo</label>
+                                <select class="custom-select" required>
+                                    <option value="">Selecionar sexo</option>
+                                    <option value="F">Femenino</option>
+                                    <option value="M">Masculino</option>
+                                </select>
+                            </div>
+                            <!-- ENTRADA PARA EL TELEFONO -->
+                            <div class="col-sm-6 my-1">
+                                <label for="example-text-input" class="col-form-label">Telefono</label>
+                                <input class="form-control" type="tel" name="nuevoNombre" placeholder="Telefono"
+                                    required autocomplete="off">
+                            </div>
                         </div>
-                        <!-- ENTRADA PARA EL APELLIDO MATERNO -->
-                        <div class="col-sm-4 my-1">
-                            <label for="example-text-input" class="col-form-label">Apellido Materno</label>
-                            <input class="form-control" type="text" name="nuevoApellidoM" placeholder="Ingresar apellido materno completo"
-                                required>
-                        </div>
+                        <hr>
                     </div>
-                    <div class="form-row align-items-center">
-                        <!-- ENTRADA PARA SELECCIONAR SU SEXO -->
-                        <div class="col-sm-6 my-1">
-                            <label class="col-form-label">Sexo</label>
-                            <select class="custom-select" required>
-                                <option value="">Selecionar sexo</option>
-                                <option value="F">Femenino</option>
-                                <option value="M">Masculino</option>
-                            </select>
-                        </div>
-                        <!-- ENTRADA PARA EL TELEFONO -->
-                        <div class="col-sm-6 my-1">
-                            <label for="example-text-input" class="col-form-label">Telefono</label>
-                            <input class="form-control" type="text" name="nuevoNombre" placeholder="Ingresar telefono"
-                                required>
-                        </div>
-                    </div>
-                </div>
                     <!--=====================================
               PIE DEL MODAL
               ======================================-->
