e5c7a4580fd4bc056cb19b179672d11eea373101
alfredoperaltag <alfredo08peralta@gmail.com>
2019-01-31 22:04:26 -0600

    ajax usuarios

--- vistas/assets/js/usuarios.js
+++ vistas/assets/js/usuarios.js
@@ -5,4 +5,18 @@ EDITAR USUARIO
 $(".btnEditarUsuario").click(function(){
     var idUsuario = $(this).attr("idusuario");
     console.log("idUsuario", idUsuario);
+    var datos = new FormData();
+    datos.append("idUsuario", idUsuario);
+    $.ajax({
+        url:"ajax/usuarios.ajax.php",
+        method: "POST",
+        data: datos,
+        cache: false,
+        contentType: false,
+        processData: false,
+        dataType: "json",
+        success: function(respuesta){
+            console.log("respuesta", respuesta);
+        }
+    });
 })
