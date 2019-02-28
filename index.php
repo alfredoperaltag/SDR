dfd41c26bcca36329b8a074b6296d8e748061e04
alfredoperaltag <alfredo08peralta@gmail.com>
2019-02-28 16:33:10 -0600

    bug

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-$ruta = str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname($_SERVER['SCRIPT_FILENAME']))."/";
-// $ruta = "http://localhost/sdr/";
+ // $ruta = str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname($_SERVER['SCRIPT_FILENAME'])) . "/";
+/* $ruta = "http://localhost/sdr/"; */
+$ruta = "C:laragon/www/SDR/";
 // echo $ruta;
-require_once $ruta."controladores/plantilla.controlador.php";
-require_once $ruta."controladores/usuarios.controlador.php";
-require_once $ruta."controladores/residentes.controlador.php";
-require_once $ruta."controladores/docentes.controlador.php";
+require_once $ruta . "controladores/plantilla.controlador.php";
+require_once $ruta . "controladores/usuarios.controlador.php";
+require_once $ruta . "controladores/residentes.controlador.php";
+require_once $ruta . "controladores/docentes.controlador.php";
 
 
-require_once $ruta."modelos/usuarios.modelo.php";
-require_once $ruta."modelos/residentes.modelo.php";
-require_once $ruta."modelos/docentes.modelo.php";
+require_once $ruta . "modelos/usuarios.modelo.php";
+require_once $ruta . "modelos/residentes.modelo.php";
+require_once $ruta . "modelos/docentes.modelo.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla->ctrPlantilla();
