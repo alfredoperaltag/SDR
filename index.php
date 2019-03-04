42c9e4d06adfefe778807a1a1fa2d600872f586b
Yonathan Román <yonathanr11.12@gmail.com>
2019-03-04 15:42:56 -0600

    update null

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 //Diferntes formas de retornar la direccion del servidor
- $ruta = str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname($_SERVER['SCRIPT_FILENAME'])) . "/";
+$ruta = str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname($_SERVER['SCRIPT_FILENAME'])) . "/";
 // $ruta = "http://localhost/sdr/";
+//$ruta = "sdriti.000webhostapp.com/";
 // $ruta = "C:laragon/www/SDR/";
+//$ruta = "";
 require_once $ruta . "controladores/plantilla.controlador.php";
 require_once $ruta . "controladores/usuarios.controlador.php";
 require_once $ruta . "controladores/residentes.controlador.php";
 require_once $ruta . "controladores/docentes.controlador.php";
 
+// echo "HOLAAAA  - ".$ruta;
 
 require_once $ruta . "modelos/usuarios.modelo.php";
 require_once $ruta . "modelos/residentes.modelo.php";
