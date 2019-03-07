c8a4c7cc8dc24c63330d48795552705945424e80
Yonathan Román <yonathanr11.12@gmail.com>
2019-03-07 17:55:59 -0600

    Add directorio

---
+++ vistas/modulos/Directorio.php
@@ -0,0 +1,9 @@
+<div class="error-area text-center">
+    <div class="container">
+        <div class="error-content">
+            <h2>DIRECTORIO</h2>
+            <p>Ooops! Algo salio mal.</p>
+            <a href="Inicio">Regresar al inicio</a>
+        </div>
+    </div>
+</div>
--- vistas/modulos/cabezoteUser.php
+++ vistas/modulos/cabezoteUser.php
@@ -37,6 +37,9 @@
                                 <li>
                                     <a href="Residentes"><i class="fa fa-users fa-2x" style="color: #845ef7;"></i><span><strong>Residentes</strong></span></a>
                                 </li>
+                                <li>
+                                    <a href="Directorio"><i class="fa fa-phone fa-2x" style="color: #845ef7;"></i><span><strong> Directorio</strong></span></a>
+                                </li>
                             </ul>
                         </nav>
                     </div>
