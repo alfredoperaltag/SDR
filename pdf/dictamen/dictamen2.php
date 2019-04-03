a6a2822483e95d5c4b912650cd6537aa7578fecb
alfredoperaltag <alfredo08peralta@gmail.com>
2019-04-02 22:47:32 -0600

    tabla responsive

--- pdf/dictamen/dictamen2.php
+++ pdf/dictamen/dictamen2.php
@@ -1,91 +1,38 @@
-<!DOCTYPE html>
-<html>
+<page backleft="12mm" backright="10mm">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <script src="main.js"></script>
-    <style>
-        img {
-            margin: 3.5% 14.5% 0% 14.5%;
-        }
-
-        h5 {
-            margin-top: -.3%;
-            text-align: center;
-        }
-
-        .sinBordeInferior {
-            border-bottom: 0.5px solid white;
-            /* border-bottom: 0.0px; */
-        }
-
-        .sinBordeSuperior {
-            border-top: 0.0px;
-
-        }
-
-        .bordeNormal {
-            border: 0.5px solid #000;
-        }
-
-        td,
-        th {
-            border: 0.5px solid #000;
-        }
-
-        #contenedor1 {
-            font-size: 11px;
-            padding: 7px 0px 23px 700px;
-            /* height: 11px; */
-            /* width: 82px; */
-            text-align: center;
-        }
-
-        #contenedor2 {
-            padding: -1px 45px;
-            font-size: 12px;
-        }
-    </style>
-</head>
-
-<body>
     <img src="../imagenes/LogoCaptura.PNG">
     <h5>INSTITUTO TECNOLÓGICO DE IGUALA <br>
         DEPARTAMENTO DE SISTEMAS Y COMPUTACIÓN <br>
         DICTAMEN DE ANTEPROYECTOS DE RESIDENCIAS PROFESIONALES
     </h5>
-    <div id="contenedor1">
-        <Table cellspacing="0" id="tabla1" class="bordeNormal">
+    <div>
+        <Table cellspacing="0" border=0.5 style="width:100%; text-align: center;" align="right">
             <tr>
-                <td class="sinBordeInferior">SEMESTRE</td>
-                <td class="bordeNormal">ENE - JUN</td>
-                <td class="bordeNormal"><?php
-                                        if ($semestre == 'EJ') {
-                                            echo $anio;
-                                        }
-                                        ?></td>
+                <td>SEMESTRE</td>
+                <td>ENE - JUN</td>
+                <td><?php
+                    if ($semestre == 'EJ') {
+                        echo $anio;
+                    }
+                    ?></td>
             </tr>
             <tr>
-                <td class="sinBordeSuperior"></td>
-                <td class="bordeNormal">AGO - DIC</td>
-                <td class="bordeNormal"><?php
-                                        if ($semestre == 'AD') {
-                                            echo $anio;
-                                        }
-                                        ?>
+                <td></td>
+                <td>AGO - DIC</td>
+                <td><?php
+                    if ($semestre == 'AD') {
+                        echo $anio;
+                    }
+                    ?>
                 </td>
             </tr>
         </Table>
     </div>
     <h5>ING. EN SISTEMAS COMPUTACIONALES</h5>
-    <div id="contenedor2">
-        <table cellspacing="0" class="bordeNormal" style="text-align: center;">
+    <div>
+        <table cellspacing="0" border=0.5 style="width:100%; text-align: center;" align="center">
             <tr>
-                <td class="sinBordeInferior"></td>
+                <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
@@ -97,7 +44,7 @@
                 <td></td>
             </tr>
             <tr>
-                <th class="sinBordeSuperior">NUM.</th>
+                <th>NUM.</th>
                 <th>CONTROL</th>
                 <th>NOMBRE DEL<br>ESTUDIANTE</th>
                 <th>S</th>
@@ -109,25 +56,27 @@
                 <th>FECHA DE<br>DICTAMEN</th>
             </tr>
             <tr>
-                <td style=" width: 29px;"><?php echo $id; ?></td>
-                <td style="width: 84px;"><?php echo $numeroControl; ?></td>
+                <td style="width:6%"><?php echo $id; ?></td>
+                <td style="width:9.5%"><?php echo $numeroControl; ?></td>
 
-                <td style="width: 143px;"><?php echo $nombre; ?></td>
-                <td style="width: 18px;"><?php if ($sexo == "Masculino") {
-                                                echo "M";
-                                            } else {
-                                                echo "F";
-                                            } ?></td>
-                <td style="width: 133px;"><?php echo $proyecto; ?></td>
-                <td style="width: 125px;"><?php echo $empresa; ?></td>
-                <td style="width: 85px;"><?php echo $asesorInterno; ?></td>
-                <td style="width: 86px;"><?php echo $asesorExterno; ?></td>
-                <td style="width: 87px;">Aceptado</td>
-                <td style="width: 94px;">16/ENERO/19</td>
+                <td style="width:15%"><?php echo $nombre; ?></td>
+                <td style="width:2%"><?php if ($sexo == "Masculino") {
+                                            echo "M";
+                                        } else {
+                                            echo "F";
+                                        } ?></td>
+                <td style="width:12.5%"><?php echo $proyecto; ?></td>
+                <td style="width:12%"><?php echo $empresa; ?></td>
+                <td style="width:11%"><?php echo $asesorInterno; ?></td>
+                <td style="width:11%"><?php echo $asesorExterno; ?></td>
+                <td style="width:11%">Aceptado</td>
+                <td style="width:11%">16/ENERO/19</td>
             </tr>
         </table>
     </div>
     <p>En caso que uno o mas Anteproyectos sean rechazados se elaborara otro registro unicamente con los anteproyectos redictaminados</p>
-</body>
-
-</html> 
+    <h5>INSTITUTO TECNOLÓGICO DE IGUALA <br>
+        DEPARTAMENTO DE SISTEMAS Y COMPUTACIÓN <br>
+        DICTAMEN DE ANTEPROYECTOS DE RESIDENCIAS PROFESIONALES
+    </h5>
+</page> 
