380797a6169db78405c67d69041e9a77a16fd034
alfredoperaltag <alfredo08peralta@gmail.com>
2019-04-05 17:08:55 -0600

    nombres imagenes

--- pdf/residencias/dictamen.php
+++ pdf/residencias/dictamen.php
@@ -1,10 +1,40 @@
 <?php
 require('../FPDF/fpdf.php');
 
-$pdf = new FPDF('L', 'mm', 'Letter');
+class PDF extends FPDF
+{
+    // Cabecera de página
+    function Header()
+    {
+        // Logo
+        $this->Image('../img/logoCaptura.png', 44, 12, 0);
+
+        // Arial bold 15
+        // $this->SetFont('Arial', 'B', 15);
+        // Movernos a la derecha
+        // $this->Cell(80);
+        // Título
+        // $this->Cell(30, 10, 'Title', 1, 0, 'C');
+        // Salto de línea
+        // $this->Ln(20);
+    }
+
+    // Pie de página
+    function Footer()
+    {
+        $this->Image('../img/iti.jpg', 44, 120, 10);
+        // $this->Image('../img/logoPequeño.png', 44, 12, 0);
+        // Posición: a 1,5 cm del final
+        //$this->SetY(-15);
+        // Arial italic 8
+        //$this->SetFont('Arial', 'I', 8);
+        // Número de página
+        //  $this->Cell(0, 10, 'Page ' . $this->PageNo() . '/{nb}', 0, 0, 'C');
+    }
+}
+
+$pdf = new PDF('L', 'mm', 'Letter');
 $pdf->AddPage();
 $pdf->SetFont('Arial', '', 12);
-$pdf->Cell(40, 10, '¡Hola, Mundo!');
-$pdf->Cell(40, 10, '¡Hola, Mundo!', 1);
-$pdf->Cell(60, 10, 'Hecho con FPDF.', 0, 1, 'C');
+
 $pdf->Output();
