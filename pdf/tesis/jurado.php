440ce471d56d862bd64f2ea1e3f627d11520dcce
Yonathan Román <yonathanr11.12@gmail.com>
2019-04-09 00:15:19 -0500

    ok 01

--- pdf/tesis/jurado.php
+++ pdf/tesis/jurado.php
@@ -1,7 +1,7 @@
 <?php
-
 require '../FPDF/fpdf.php';
-
+require '../../controladores/residentes.controlador.php';
+require '../../modelos/residentes.modelo.php';
 class PDF extends FPDF
 {
     //Cabecera
@@ -10,9 +10,9 @@ class PDF extends FPDF
         $this->SetFont('Arial', 'B', '10');
         // $this->Image('../img/sep-logo.jpg', 20, 2, 67, 52, 'JPG');
         $this->Image('../img/sepNew_R.png', 15, 12, 67, 25, 'PNG');
-
         $this->Image('../img/TecNacMex.PNG', 137, 12, 66, 24, 'PNG');
-        $this->Cell(0, 46, '', 0, 1, 'C');
+        $this->Cell(0, 46, '', 0, 1, 'C');//NOTE no borrar
+        // $this->Cell(80, 25, 'gggggggggggggggg', 1, 1, 'C');
         // $this->Ln(20);
     }
     public function Footer()
@@ -24,24 +24,26 @@ class PDF extends FPDF
         $x = $this->GetX();
         $y = $this->GetY();
         $this->Image('../img/iti.jpg', 15, 253, 12);
-        $this->SetFont('Helvetica', '', '7.5');
-        $this->SetXY($x + 20, $y-12);
-        $this->Cell(20, 4, utf8_decode('Carretera Nacional Iguala-Taxco esquina Periférico Norte, Col. Adolfo López Mateos Infonavit, C.P. 40030'), 0, 1, 'L');
-        $this->Cell(20);
-        $this->Cell(20, 4, utf8_decode('Iguala de la Independencia, Gro. Tels. (733) 3321425 Ext. 225,'), 0, 1, 'L');
-        $this->Cell(44);
-        $this->Cell(20, 4, utf8_decode('www.itiguala.edu.mx         e-mail:'), 0, 0, 'L');
-        $this->SetFont('Helvetica', 'BU', '7.5');
-        $this->Cell(20);
-        $this->Cell(65, 4, utf8_decode('sistemas@itiguala.edu.mx'), 0, 0, 'L');
+        $this->SetFont('Helvetica', '', '7');
+        $this->SetXY($x, $y - 12);
+        $this->Cell(0, 4, utf8_decode('Carretera Nacional Iguala-Taxco esquina Periférico Norte, Col. Adolfo López  Mateos  Infonavit, C.P. 40030, '), 0, 1, 'C');
+        // $this->Cell(20);
+        $this->Cell(0, 4, utf8_decode('Iguala de la Independencia, Gro. Tels. (733) 3321425'), 0, 1, 'C');
+        // $this->Cell(44);
+        $this->Cell(0, 4, utf8_decode('Ext. 225, e-mail: comunicacion@itiguala.edu.mx,'), 0, 1, 'C');
+        $this->SetFont('Helvetica', 'B', '7');
+        // $this->Cell(20);
+        $this->Cell(0, 4, utf8_decode('sistemas@itiguala.edu.mx'), 0, 0, 'C');
+        // $x = $this->GetX();
+        $this->Image('../img/iso14001.jpg', 155 + 12, 253, 17);
         $x = $this->GetX();
-        $this->Image('../img/iso14001.jpg', $x+12, 253, 12);
-        $x = $this->GetX();
-        $this->Image('../img/iso9001.jpg', $x+31, 253, 12);
-
+        // $this->Image('../img/iso9001.jpg', $x + 31, 253, 12);
+        $this->Image('../img/norma.jpg', 155 + 31, 253, 15);
     }
 }
-
+$item = "id";
+$valor = $_GET['id'];
+$res = ControladorResidentes::ctrMostrarInfoResidentes($item, $valor);
 $pdf = new PDF('P', 'mm', 'Letter');
 $pdf->AddPage();
 // $pdf->Image('../img/fondo1.png', '28', '99', '150', '150', 'PNG');
@@ -49,7 +51,11 @@ $h = $pdf->GetPageHeight();
 $w = $pdf->GetPageWidth();
 // $pdf->Image('../img/fondo_membrete.jpg', '0', '28', '210', '275', 'JPG');
 $pdf->Image('../img/fondo_membrete_R.jpg', '0', '38', '220', '243', 'JPG');
+// $pdf->Image('../img/aaa.jpg', '0', '38', '220', '243', 'JPG');
 // $pdf->Image('../img/fondo_membrete.jpg', 0, 28, 50);
+$pdf->SetFont('Helvetica', '', '7');
+$pdf->Cell(0, 4, utf8_decode('"2019, Año del Caudillo del Sur, Emiliano Zapata"'), 0, 1, 'C');
+$pdf->Ln(5); 
 $pdf->SetFont('Helvetica', '', '9');
 $pdf->Cell(107);
 $pdf->Cell(80, 4, 'OF. No. DSC-ITI/1031/2018', 0, 1, 'L');
@@ -63,8 +69,7 @@ $pdf->Cell(25, 4, 'IGUALA, GRO., ', 0, 0, 'L');
 // AQUI VA LA FECHA 2019-Abril-05
 $pdf->SetFont('Helvetica', 'B', '9');
 $pdf->Cell(50, 4, ' 2018-septiembre-28', 0, 1, 'L');
-
-$pdf->Ln(15); //CELDA DE ESPACIO
+$pdf->Ln(12); //CELDA DE ESPACIO
 $pdf->SetFont('Helvetica', 'B', '9');
 $pdf->Cell(19);
 $pdf->Cell(80, 4, 'M.D.I.S. SILVIA VALLE BAHENA', 0, 1, 'L');
@@ -72,24 +77,31 @@ $pdf->Cell(19);
 $pdf->Cell(80, 4, utf8_decode('DOCENTE DE ESTA INSTITUCIÓN.'), 0, 1, 'L');
 $pdf->Cell(19);
 $pdf->Cell(80, 4, 'P R E S E N T E', 0, 1, 'L');
-
 $pdf->Ln(7); //CELDA DE ESPACIO
 $pdf->SetFont('Helvetica', '', '9');
 $pdf->Cell(31);
 $pdf->Cell(0, 4, 'Por medio del presente, me permito hacer de su conocimiento que ha sido comisionado (a) para ', 0, 1, 'L');
 $pdf->Cell(18);
 $pdf->Cell(0, 4, utf8_decode('llevar a cabo la Revisión del Trabajo de Titulación. '), 0, 1, 'L');
-
 $pdf->Ln(3); //CELDA DE ESPACIO
 $pdf->Cell(23);
-$pdf->Cell(28, 4, utf8_decode(' Alumno (s):'), 1, 0, 'L');
-$pdf->SetFont('Helvetica', 'B', '9');
+$x = $pdf->GetX();
+$y = $pdf->GetY();
+// $pdf->Cell(28, 4, utf8_decode(' Alumno (s):'), 1, 0, 'L');
+$pdf->MultiCell(28, 4, utf8_decode(' Alumno (s):                            '), 1, 'L');
 // AQUI VA EL NOMBRE DEL ALUMNO
-$pdf->Cell(70, 4, utf8_decode(' MARÍA VALENTINA HERNANDEZ RAMIREZ'), 1, 0, 'L');
+$pdf->SetXY($x + 28, $y);
+$x = $pdf->GetX();
+$y = $pdf->GetY();
+// $pdf->Cell(70, 4, utf8_decode(strtoupper ($res['nombre'])), 1, 0, 'L');
+$pdf->SetFont('Helvetica', 'B', '9');
+$pdf->MultiCell(70, 4, utf8_decode(' '.strtoupper ($res['nombre']).'                                        '), 1, 'L');
 // AQUI VA LA CARRERA
 $pdf->SetFont('Helvetica', '', '9');
 // NOTE Checar espacio de carrera
-$pdf->Cell(70, 4, utf8_decode(' Área: INGENIERIA INFORMATICA'), 1, 1, 'L');
+$pdf->SetXY($x + 70, $y);
+// $pdf->Cell(70, 4, utf8_decode(' Área: ' . strtoupper ($res['carrera'])), 1, 1, 'L');
+$pdf->MultiCell(70, 4, utf8_decode(' '.strtoupper ($res['carrera'])), 1, 'L');
 // $pdf->MultiCell(50, 4, utf8_decode('Área: INGENIERIA EN SISTEMAS COMPUTACIONALES'), 1, 'L');
 $pdf->Cell(23);
 $pdf->Cell(28, 4, utf8_decode(' Opción'), 1, 0, 'L');
@@ -102,25 +114,23 @@ $y = $pdf->GetY();
 $pdf->MultiCell(28, 4, utf8_decode(' Proyecto:                    '), 1, 'L');
 $pdf->SetXY($x + 28, $y);
 // $pdf->Cell(53);
-$pdf->MultiCell(140, 4, utf8_decode(' DETERMINACION DE LOS DISPOSITIVOS DE RED QUE PERMITAN PROPORCIONAR SERVICIO DE INTERNET EN EL INSTITUTO TECNOLOGICO DE IGUALA'), 1, 'L');
+$pdf->MultiCell(140, 4, utf8_decode(' '.strtoupper ($res['nombreProyecto'])), 1, 'L');
 $pdf->Cell(23);
 $pdf->Cell(28, 4, utf8_decode(' Asesor:'), 1, 0, 'L');
-$pdf->Cell(140, 4, utf8_decode(' LIC. ENRIQUE MENA SALGADO'), 1, 1, 'L');
+$pdf->Cell(140, 4, utf8_decode(' '.strtoupper ($res['asesorInt'])), 1, 1, 'L');
 $pdf->Cell(23);
 $pdf->Cell(28, 4, utf8_decode(' Revisor 1:'), 1, 0, 'L');
-$pdf->Cell(140, 4, utf8_decode(' M.D.I.S. SILVIA VALLE BAHENA'), 1, 1, 'L');
+$pdf->Cell(140, 4, utf8_decode(' '.strtoupper ($res['revisor1'])), 1, 1, 'L');
 $pdf->Cell(23);
 $pdf->Cell(28, 4, utf8_decode(' Revisor 2:'), 1, 0, 'L');
-$pdf->Cell(140, 4, utf8_decode(' M.A. ANGELITA DIONICIO ABRAJAN'), 1, 1, 'L');
+$pdf->Cell(140, 4, utf8_decode(' '.strtoupper ($res['revisor2'])), 1, 1, 'L');
 $pdf->Cell(23);
 $pdf->Cell(45, 4, utf8_decode(' Documentos entregados:'), 1, 0, 'L');
 $pdf->Cell(123, 4, utf8_decode(' 1 EJEMPLAR PARA CADA REVISOR'), 1, 1, 'L');
-
 $pdf->Ln(6); //CELDA DE ESPACIO
 // $pdf->Cell(37);
 $pdf->SetFont('Helvetica', 'BIU', '9');
 $pdf->Cell(0, 4, utf8_decode('Los revisores deberán ponerse en contacto para unificar criterios, y emitir un solo dictamen.'), 0, 1, 'C');
-
 $pdf->Ln(8); //CELDA DE ESPACIO
 $pdf->SetFont('Helvetica', '', '9');
 $pdf->Cell(31);
@@ -133,22 +143,18 @@ $pdf->SetFont('Helvetica', '', '9');
 $pdf->Cell(39, 4, utf8_decode('en el entendido que de no'), 0, 1, 'L');
 $pdf->Cell(21);
 $pdf->Cell(0, 4, utf8_decode('cumplir dentro de este plazo, se estará imposibilitado a que se continúe con los trámites sucesivos.'), 0, 1, 'L');
-
 $pdf->Ln(2); //CELDA DE ESPACIO
 $pdf->SetFont('Helvetica', '', '9');
 $pdf->Cell(30);
 $pdf->Cell(80, 4, utf8_decode('Con la seguridad de su oportuna entrega, quedo de usted. '), 0, 1, 'L');
-
 $pdf->Ln(7); //CELDA DE ESPACIO
 $pdf->SetFont('Helvetica', 'B', '9');
 $pdf->Cell(0, 4, utf8_decode('A T E N T A M E N T E'), 0, 1, 'C');
 $pdf->Cell(0, 4, utf8_decode('"TECNOLOGÍA COMO SINÓNIMO DE INDEPENDENCIA"'), 0, 1, 'C');
-
-$pdf->Ln(10);
+$pdf->Ln(15);
 $pdf->SetFont('Helvetica', 'B', '9');
 $pdf->Cell(0, 4, utf8_decode('ING. JORGE EDUARDO ORTEGA LOPEZ'), 0, 1, 'C');
 $pdf->Cell(0, 4, utf8_decode('JEFE DEL DEPTO. DE SISTEMAS Y COMPUTACIÓN'), 0, 1, 'C');
-
 $pdf->Ln(16); //CELDA DE ESPACIO
 $pdf->SetFont('Helvetica', '', '9');
 $pdf->Cell(20);
@@ -156,5 +162,4 @@ $pdf->Cell(0, 4, utf8_decode('C.C.P. ARCHIVO.'), 0, 1, 'L');
 $pdf->SetFont('Helvetica', '', '6');
 $pdf->Cell(20);
 $pdf->Cell(25, 4, utf8_decode('*SRZB*ere'), 0, 1, 'R');
-
-$pdf->Output('I','Jurado_Seleccionado.pdf');
+$pdf->Output('I', 'Jurado_Seleccionado.pdf');
