e8bc5293c5e0f89806f14b5352dd917ea8dd3914
alfredoperaltag <alfredo08peralta@gmail.com>
2019-04-23 22:16:23 -0500

    texto diferentes tipos de letra script

--- pdf/residencias/sinodales.php
+++ pdf/residencias/sinodales.php
@@ -36,6 +36,48 @@ class PDF extends FPDF
         // $this->Image('../img/iso9001.jpg', $x + 31, 253, 12);
         $this->Image('../img/norma.jpg', 155 + 31, 253, 15);
     }
+    function WriteText($text)
+    {
+        $intPosIni = 0;
+        $intPosFim = 0;
+        if (strpos($text, '<') !== false && strpos($text, '[') !== false) {
+            if (strpos($text, '<') < strpos($text, '[')) {
+                $this->Write(4, substr($text, 0, strpos($text, '<')));
+                $intPosIni = strpos($text, '<');
+                $intPosFim = strpos($text, '>');
+                $this->SetFont('', 'B');
+                $this->Write(4, substr($text, $intPosIni + 1, $intPosFim - $intPosIni - 1));
+                $this->SetFont('', '');
+                $this->WriteText(substr($text, $intPosFim + 1, strlen($text)));
+            } else {
+                $this->Write(4, substr($text, 0, strpos($text, '[')));
+                $intPosIni = strpos($text, '[');
+                $intPosFim = strpos($text, ']');
+                $w = $this->GetStringWidth('a') * ($intPosFim - $intPosIni - 1);
+                $this->Cell($w, $this->FontSize + 0.75, substr($text, $intPosIni + 1, $intPosFim - $intPosIni - 1), 1, 0, '');
+                $this->WriteText(substr($text, $intPosFim + 1, strlen($text)));
+            }
+        } else {
+            if (strpos($text, '<') !== false) {
+                $this->Write(4, substr($text, 0, strpos($text, '<')));
+                $intPosIni = strpos($text, '<');
+                $intPosFim = strpos($text, '>');
+                $this->SetFont('', 'B');
+                $this->WriteText(substr($text, $intPosIni + 1, $intPosFim - $intPosIni - 1));
+                $this->SetFont('', '');
+                $this->WriteText(substr($text, $intPosFim + 1, strlen($text)));
+            } elseif (strpos($text, '[') !== false) {
+                $this->Write(4, substr($text, 0, strpos($text, '[')));
+                $intPosIni = strpos($text, '[');
+                $intPosFim = strpos($text, ']');
+                $w = $this->GetStringWidth('a') * ($intPosFim - $intPosIni - 1);
+                $this->Cell($w, $this->FontSize + 0.75, substr($text, $intPosIni + 1, $intPosFim - $intPosIni - 1), 1, 0, '');
+                $this->WriteText(substr($text, $intPosFim + 1, strlen($text)));
+            } else {
+                $this->Write(4, $text);
+            }
+        }
+    }
 }
 
 $item = "id";
@@ -86,39 +128,12 @@ $pdf->Cell(0, 0, utf8_decode('P R E S E N T E .'), 0, 0, 'L');
 $pdf->Ln(8);
 $pdf->Cell(0, 0, utf8_decode('AT´N: COORDINADORA DE TITULACIÓN.'), 0, 0, 'L');
 $pdf->Ln(12);
-$pdf->SetFont('Helvetica', '', '8');
-$pdf->Cell(0, 0, utf8_decode('Por medio del presente, me permito enviar a usted el '), 0, 1, 'L');
-$pdf->SetFont('Helvetica', 'BI', '8');
-$pdf->Cell(149, 0, utf8_decode('JURADO '), 0, 1, 'C');
-$pdf->SetFont('Helvetica', '', '8');
-$pdf->Cell(133, 0, utf8_decode('que fungirá en el Acto de Titulación, del '), 0, 0, 'R');
-$pdf->Ln(4);
-$pdf->SetFont('Helvetica', 'B', '8');
-$pdf->Cell(0, 0, utf8_decode(utf8_decode('C. ' .  strtoupper($nombre) . ',')), 0, 1, 'L');
-$pdf->SetFont('Helvetica', '', '8');
-$pdf->Cell(149, 0, utf8_decode(' que presenta su protocolo para su '), 0, 1, 'C');
-$pdf->SetFont('Helvetica', 'B', '8');
-$pdf->Cell(227, 0, utf8_decode('TITULACIÓN INTEGRAL,'), 0, 1, 'C');
-$pdf->SetFont('Helvetica', '', '8');
-$pdf->Ln(4);
-$pdf->Cell(0, 0, utf8_decode('el día'), 0, 1, 'L');
-$pdf->SetFont('Helvetica', 'B', '8');
-$pdf->Cell(8);
-$pdf->Cell(0, 0, utf8_decode(strtoupper($fechaTitulacion)), 0, 1, 'L');
-
-
 
 $pdf->SetFont('Helvetica', '', '8');
-// $pdf->Cell(8);
-$anchoFechaTitulacion = $pdf->GetStringWidth($fechaTitulacion);
-// $pdf->Cell($anchoFechaTitulacion);
-// $pdf->Cell(5);
-// $pdf->SetX($anchoFechaTitulacion + 20);
-// $pdf->Cell();
-$pdf->SetX(18 + $anchoFechaTitulacion + 4.5);
-$x2 = $pdf->GetX();
-$pdf->Cell(0, 0, utf8_decode('del año en curso, a las'), 0, 1, 'L');
-// $pdf->Cell(0, 0, utf8_decode($anchoFechaTitulacion), 0, 1, 'C');
-$pdf->Cell(0, 0, utf8_decode($x2), 0, 1, 'C');
+$text = "Por medio del presente, me permito enviar a usted el <JURADO> que fungirá en el Acto de Titulación, del 
+<C. " . strtoupper($nombre) . ",> que presenta su protocolo para su <TITULACIÓN INTEGRAL,> 
+el día <" . strtoupper($fechaTitulacion) . "> del año en curso, a las <10:00 hrs.>, en la <SALA DE TITULACIÓN YOHUALCEHUATL.>";
+$pdf->WriteText(utf8_decode($text));
+
 
 $pdf->Output('I', 'Asignación de Sinodales.pdf', 'D');
